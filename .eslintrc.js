12abdb6766a1d11054f725cc5f43428e0ea84a8c
Daniil Kuznetsov <dankuzi@yandex.ru>
2023-07-24 22:37:20 +0400

    test

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': [
+    'google',
+    'plugin:react/recommended',
+    "extends: airbnb-base",
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    "no-underscore-dangle": ["error", { "allow": ["_id"] }]
+  },
+};
