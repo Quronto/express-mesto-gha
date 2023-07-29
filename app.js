37c0290870eaa5a6e86f1ef4cea49106009b3ac4
Daniil Kuznetsov <dankuzi@yandex.ru>
2023-07-30 01:52:15 +0400

    test1

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
+
+const { PORT = 7878 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64a7148363630adcce62b10a',
+  };
+
+  next();
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
